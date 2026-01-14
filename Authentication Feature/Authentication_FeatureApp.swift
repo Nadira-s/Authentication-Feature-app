f32e78b872ba3ba5b6679413d578f1717f04d178
Nadira Seitkazy <nadiraseitkazu@gmail.com>
2026-01-14 10:52:38 +0500

    Initial Commit

---
+++ Authentication Feature/Authentication_FeatureApp.swift
@@ -0,0 +1,17 @@
+//
+//  Authentication_FeatureApp.swift
+//  Authentication Feature
+//
+//  Created by Nadira Seitkazy  on 14.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct Authentication_FeatureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
